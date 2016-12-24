4a3967b151b628d0f7471bdc91ceb066c10756f2
Mohamedji Bardolia <mohamedji@gmail.com>
2016-12-24 14:22:50 +0530

    Update deply.sh

--- deply.sh
+++ deply.sh
@@ -2,12 +2,11 @@
 echo "Deploying Openstack Newton please wait..."
 echo "Installing dependencies..."
 apt-get update
-apt-get install lvm2 git software-properties-common python-software-properties -y
+apt-get install lvm2 docker.io software-properties-common python-software-properties -y
 add-apt-repository cloud-archive:newton -y
 apt-get install openvswitch-switch -y
 docker run -itd -p 80:80 -p 6080:6080 --privileged --device=/dev/sdb:/dev/sdb -v /var/run/lvm/lvmetad.socket:/var/run/lvm/lvmetad.socket -v /lib/modules/:/lib/modules mohamedji/openstack /bin/bash
 sleep 1m
-ip = (/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
-echo "Connect to Openstack on http://"+ip+"/horizon"
+echo "Connect to Openstack on http://`(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)`/horizon"
 echo "Username:admin"
 echo "Password:admin_pass"
