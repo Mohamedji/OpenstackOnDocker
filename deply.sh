8050b5c0dd7db26446797eeafbeffdc8750f41d9
Mohamedji Bardolia <mohamedji@gmail.com>
2016-12-26 11:27:46 +0530

    Update deply.sh

--- deply.sh
+++ deply.sh
@@ -5,7 +5,6 @@ apt-get update
 apt-get install lvm2 docker.io software-properties-common python-software-properties -y
 add-apt-repository cloud-archive:newton -y
 apt-get install openvswitch-switch -y
-docker build -t openstack .
 docker run -itd -p 80:80 -p 6080:6080 --privileged --device=/dev/sdb:/dev/sdb -v /var/run/lvm/lvmetad.socket:/var/run/lvm/lvmetad.socket -v /lib/modules/:/lib/modules mohamedji/openstack /bin/bash
 echo "Have a coffee & come back Openstack will be ready for you...."
 sleep 5m
